4eb8d3d3458cffb1c3473e309b9c7ce5aa28b36a
Dihep <dihep.demayo@gmail.com>
2024-05-07 10:29:02 +0200

    M1- Creata funzione per generare bombe non ripetute.

--- js/script.js
+++ js/script.js
@@ -3,9 +3,10 @@ const container = document.getElementById("container");
 const startBtn = document.getElementById("start");
 
 //Creazione griglia al click del bottone
+let bombe = [];
 startBtn.addEventListener("click", creaGriglia)
 
-//Funzioni
+//Funzione principale
 function creaGriglia() {    
     for (let i = 0; i < 100; i++) {
         //Creazione nuova cella
@@ -18,18 +19,39 @@ function creaGriglia() {
         container.append(cella);
         console.log(`cella ${i + 1} creata.`);
     }
+    bombe = generaBombe();
+    debugger;
     startBtn.classList.add("hidden");
 };
 
+//Funzioni di supporto
 function impostaCella(cella, i) {
     cella.classList.add("cella");
     console.log(cella, cella.classList);
     cella.innerText = i + 1;
     return(cella);
 };
-
 function clickCella() {
     numCella = this.innerText;
     console.log("Click su cella ", numCella);
     this.classList.add("clicked");
+};
+function generaBombe() {
+    let lista = [Math.floor(Math.random()*16)];
+    for (let i = 1; i < 16; i++) {
+        //Creo numero casuale
+        lista[i] = Math.floor(Math.random()*16)    
+        //Controllo che non sia presente tra quelli già generati
+        doubleCheck = false;
+        for (let p = 0; p < i; p++) {
+            if (lista[i] == lista[p]) {
+                doubleCheck = true
+            };
+        };
+        //Se già presente, decremento contatore per ripetere un iterazione del ciclo
+        if (doubleCheck == true) {
+            i--;
+        }
+    };
+    return(lista);
 };
